8d2a6ea4f1974d881ffebc79ca5bbc860ab61a54
xomya40k <xomya4ok2@gmail.com>
2024-08-08 18:28:26 +0300

    Added: URL validation in `AudioDownloader` and `VideoDownloader` Updated: error messages for invalid URLs and unsupported file format

--- src/Downloader/AudioDownloader.php
+++ src/Downloader/AudioDownloader.php
@@ -18,12 +18,16 @@ final class AudioDownloader {
     {
         $audio = null;
 
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException('Invalid URL');
+        }
+
         try {
             $audio = $this->ffmpeg->open($url);
         } catch (\RuntimeException $e) {
             throw new \RuntimeException('Failed to download file: ' . $e->getCode());
         } catch (\InvalidArgumentException $e) {
-            throw new \InvalidArgumentException('Invalid url. Unsupported file format');
+            throw new \InvalidArgumentException('Unsupported file format');
         }
 
         if ($audio instanceof Video) {
--- src/Downloader/VideoDownloader.php
+++ src/Downloader/VideoDownloader.php
@@ -18,15 +18,19 @@ final class VideoDownloader {
     {
         $video = null;
 
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException('Invalid URL');
+        }
+
         try {
             $video = $this->ffmpeg->open($url);
         } catch (\RuntimeException $e) {
             throw new \RuntimeException('Failed to download file: ' . $e->getCode());
         } catch (\InvalidArgumentException $e) {
-            throw new \InvalidArgumentException('Invalid url. Unsupported file format');
+            throw new \InvalidArgumentException('Unsupported file format');
         }
 
-        if ($video instanceof Audio) {
+        if (!($video instanceof Video)) {
             throw new \RuntimeException('Expected video file, got audio');
         }
 
